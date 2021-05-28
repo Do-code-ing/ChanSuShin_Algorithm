85d6d417a525e15c28f448c5b74b9bbbe4df576b
Do-code-ing <zxcvbn658@naver.com>
2021-05-28 22:57:56 +0900

    2021-05-28

---
+++ practice.py
@@ -0,0 +1,24 @@
+from math import *
+import sys
+input = sys.stdin.readline
+
+def find_m(arr, n):
+    answer = set()
+    temp_set = set()
+    for i in range(n-1):
+        temp_set.add(arr[i+1]-arr[i])
+    gcd_ = gcd(*temp_set)
+    for i in range(2, int(gcd_**0.5)+1):
+        if gcd_ % i == 0:
+            answer.add(i)
+            answer.add(gcd_//i)
+    answer.add(gcd_)
+    return sorted(answer)
+
+def solution():
+    n = int(input())
+    arr = [int(input()) for _ in range(n)]
+    arr.sort()
+    print(" ".join(map(str, find_m(arr, n))))
+
+solution()
